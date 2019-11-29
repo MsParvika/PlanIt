cdce6a8b40b7d11f56ce65acb2c36cb087ff66e8
Rohan Passi <rpassi@asu.edu>
2019-11-28 18:14:53 -0700

    adding client id

--- app/src/main/java/com/example/planit/LoginActivity.java
+++ app/src/main/java/com/example/planit/LoginActivity.java
@@ -39,6 +39,7 @@ public class LoginActivity extends AppCompatActivity {
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.server_client_id))
                 .requestEmail()
                 .build();
         // Build a GoogleSignInClient with the options specified by gso.
